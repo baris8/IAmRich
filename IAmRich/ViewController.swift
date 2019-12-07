5091471422fa28dc3cc0fecb0ed5b55b2d413ac3
uectas <baris.uectas@uni-duesseldorf.de>
2019-12-07 01:13:35 +0100

    Initial Commit

---
+++ IAmRich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  IAmRich
+//
+//  Created by Baris Uectas on 07.12.19.
+//  Copyright © 2019 Baris Uectas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
